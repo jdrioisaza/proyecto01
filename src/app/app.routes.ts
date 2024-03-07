d2d44e312a2ecfaa2e2d16a4fac64cd07d7d616f
Mauricio Polo <mauriciopoloem@gmail.com>
2024-03-06 22:25:33 -0500

    Se agregaron modulos de login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { MenuComponent } from './componentes/menu/menu.component';
 import { OtroComponent } from './componentes/otro/otro.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { SCRUMTeamComponent } from './componentes/proyectos/scrumteam/scrumteam.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { SingUpComponent } from './componentes/sing-up/sing-up.component';
 
 export const routes: Routes = [
   { path: 'home', component: InicioComponent },
@@ -13,7 +15,8 @@ export const routes: Routes = [
   { path: 'other', component: OtroComponent },
   { path: 'about', component: AcercaDeComponent },
   { path: 'scrum', component: SCRUMTeamComponent },
-
+  { path: 'login', component: LoginComponent},
+  { path: 'signUp', component: SingUpComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: ErrorComponent },
 ];
